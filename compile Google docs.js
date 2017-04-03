ab1a754e7e2751f7774cc21845fdd2778277a59b
Willem Mulder <willemmulder@gmail.com>
2017-04-03 21:30:24 +0200

    Improved compiler for Internet sources

--- compile Google docs.js
+++ compile Google docs.js
@@ -3,6 +3,11 @@ var fs = require('fs');
 
 // Map URLS to files
 var mappings = {
+    "eindgebruiker_opstarten" : {
+        source : "https://docs.google.com/document/d/1D7QTcXbeewTsrnOXfYNOscQc7ZMgyPDlSexxS06YpNU/pub",
+        filters : ["googledocs"],
+        target : "./_includes/external/eindgebruiker_opstarten.html"
+    },
     "technisch_profiel_aanmaken" : {
         source : "https://docs.google.com/document/d/1jjcx74wxlLCsOotm4ZSRnKA54sVdtaUfAg_kUr0P1Gk/pub",
         filters : ["googledocs"],
@@ -13,12 +18,39 @@ var mappings = {
 // Define filters 
 var filters = {
     "googledocs" : function(input) {
+        // Get proper HTML
         var startToken = '<div id="contents">';
         var endToken = '</div><div id="footer">';
         var startIndex = input.indexOf(startToken) + startToken.length;
         var endIndex = input.indexOf(endToken);
-        var extraHTML = '<style>.filterwrapper table{ width:100%; border: 0px; } .filterwrapper tr, .filterwrapper td { border: 0px; }</style>';
-        return extraHTML + '<div class="filterwrapper">' + input.substring(startIndex, endIndex) + '</div>';
+        var HTML = input.substring(startIndex, endIndex);
+        // Proper H2 tags
+        HTML = HTML.replace(/<h2.*?>(.*?)<\/h2>/g, "<h2 class='section-heading'>$1</h2>");
+        // Replace H3 with p tag 
+        HTML = HTML.replace(/<h3.*?>(.*?)<\/h3>/g, "<p class='lead'>$1</p>");
+        // Replace table with nice divs
+        HTML = HTML.replace(/<table.*?>(.*?)<\/table>/g, "<div>$1</div>");
+        HTML = HTML.replace(/<tr.*?>(.*?)<\/tr>/g, "<div class='content-section-a'><div class='container'><div class='row'>$1</div></div></div>");
+        HTML = HTML.replace(/<td.*?>(.*?)<\/td>/g, "<div class='col-lg-6 col-sm-6'>$1</div>");
+        var sectionToken = "<div class='content-section-a'>";
+        var sectionIndex = HTML.indexOf(sectionToken);
+        var counter = 0;
+        while(sectionIndex > -1) {
+            if (counter % 2 == 1) {
+                HTML = HTML.substring(0,sectionIndex) + "<div class='content-section-b'>" + HTML.substring(sectionIndex+sectionToken.length);
+            }
+            sectionIndex = HTML.indexOf(sectionToken, sectionIndex+1);
+            counter++;
+        }
+        // Create proper link tags
+        // Markdown syntax like [Ga hiernaartoe!](nl/documentatie/eindgebruiker/app)
+        HTML = HTML.replace(/\[(.+?)\]\((.*?)\)/g, '<a href="{% include link id="$2" %}">$1</a>');
+        // Remove style tag
+        HTML = HTML.replace(/<style.*?>(.*?)<\/style>/g, "");
+        // Add style tag
+        var styleHTML = '<style>.filterwrapper table{ width:100%; border: 0px; } .filterwrapper tr, .filterwrapper td { border: 0px; }</style>';
+        // Return in wrapper
+        return styleHTML + '<div class="filterwrapper">' + HTML + '</div>';
     }
 }
 
